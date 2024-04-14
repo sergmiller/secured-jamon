ae67a161c533571582ca37e96964dc3a48f6f135
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2024-04-14 11:37:11 +0200

    bump app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,10 @@ import { BitcoinView } from "./components/Bitcoin.jsx";
 
 // CONSTANTS
 const MPC_CONTRACT = 'multichain-testnet-2.testnet';
+const MARKET_CONTRACT = 'cross-contract-jamon8.testnet';
 
 // NEAR WALLET
-const wallet = new Wallet({ network: 'testnet', createAccessKeyFor: MPC_CONTRACT });
+const wallet = new Wallet({ network: 'testnet', createAccessKeyFor: MARKET_CONTRACT });
 
 function App() {
   const [isSignedIn, setIsSignedIn] = useState(false);
@@ -28,33 +29,41 @@ function App() {
     <>
       <Navbar wallet={wallet} isSignedIn={isSignedIn}></Navbar>
       <div className="container">
-        <h4> 🔗 NEAR Multi Chain </h4>
+        <h4> 🔗 Jamon Swap </h4>
         <p className="small">
-          Safely control accounts on other chains through the NEAR MPC service. Learn more in the <a href="https://docs.near.org/abstraction/chain-signatures"> <b>documentation</b></a>.
+          Safely swap ERC20 coins between Different Chains.<br/>
+          DAI on Ethereum on USDC on Near.
         </p>
 
         {isSignedIn &&
-          <div style={{ width: '50%', minWidth: '400px' }}>
+            <div style={{width: '50%', minWidth: '400px'}}>
 
-            <div className="input-group input-group-sm mt-3 mb-3">
-              <input className="form-control text-center" type="text" value={`MPC Contract: ${MPC_CONTRACT}`} disabled />
+              <div className="input-group input-group-sm mt-3 mb-3">
+                <input className="form-control text-center" type="text" value={`MPC Contract: ${MPC_CONTRACT}`}
+                       disabled/>
+              </div>
+
+              <div className="input-group input-group-sm mt-3 mb-3">
+                <input className="form-control text-center" type="text"
+                       value={`Jamon Market Contract: ${MARKET_CONTRACT}`} disabled/>
+              </div>
+
+              <div className="input-group input-group-sm my-2 mb-4">
+                <span className="text-primary input-group-text" id="chain">Role</span>
+                <select className="form-select" aria-describedby="chain" value={chain}
+                        onChange={e => setChain(e.target.value)}>
+                  <option value="eth"> Ξ Seller</option>
+                  <option value="btc"> Buyer</option>
+                </select>
+              </div>
+
+              {chain === 'eth' && <EthereumView props={{setStatus, wallet, MPC_CONTRACT, MARKET_CONTRACT}}/>}
+              {chain === 'btc' && <BitcoinView props={{setStatus, wallet, MPC_CONTRACT, MARKET_CONTRACT}}/>}
             </div>
-
-            <div className="input-group input-group-sm my-2 mb-4">
-              <span className="text-primary input-group-text" id="chain">Chain</span>
-              <select className="form-select" aria-describedby="chain" value={chain} onChange={e => setChain(e.target.value)} >
-                <option value="eth"> Ξ Ethereum </option>
-                <option value="btc"> ₿ BTC </option>
-              </select>
-            </div>
-
-            {chain === 'eth' && <EthereumView props={{ setStatus, wallet, MPC_CONTRACT }} />}
-            {chain === 'btc' && <BitcoinView props={{ setStatus, wallet, MPC_CONTRACT }} />}
-          </div>
         }
 
         <div className="mt-3 small text-center">
-          <span> {status} </span>
+        <span> {status} </span>
         </div>
       </div>
     </>
