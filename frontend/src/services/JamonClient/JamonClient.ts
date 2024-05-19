d659dfcabd0c5094e62f45c60309bf36cd5939db
AlcibiadesCleinias <ivan.cheprasov@phystech.edu>
2024-05-19 12:54:29 +0200

    deprecate mpc path - use salt

--- frontend/src/services/JamonClient/JamonClient.ts
+++ frontend/src/services/JamonClient/JamonClient.ts
@@ -9,10 +9,8 @@ const MPC_PUBLIC_KEY = import.meta.env.VITE_MPC_PUBLIC_KEY ?? "";
 const NEAR_ACCOUNT_ID = import.meta.env.VITE_NEAR_ACCOUNT_ID ?? "";
 
 const NEAR_PROXY_ACCOUNT_ID = import.meta.env.VITE_NEAR_PROXY_ACCOUNT_ID ?? "";
-// TODO: it should not be hardhcoded as it is salt.
-const MPC_PATH = import.meta.env.VITE_MPC_PATH ?? "";
 // TODO: assert on all envs.
-console.log(MPC_PUBLIC_KEY, MPC_PATH)
+console.log(MPC_PUBLIC_KEY)
 
 export class JamonSwapClient {
     // Kinda engine for Near chain transaction {custom wallet, near account}.
@@ -24,18 +22,19 @@ export class JamonSwapClient {
     // Generate Derived Address in the Ethereum Network
     // It is aka Offer Id.
     // TODO: add ethereum desired address as salt.
-    async getDerivedEthAddress() {
+    async getDerivedEthAddress(mpcPath) {
         const {address} = await generateAddress({
             publicKey: MPC_PUBLIC_KEY,
             accountId: NEAR_PROXY_ACCOUNT_ID,
-            path: MPC_PATH,
+            path: mpcPath,
             chain: "ethereum",
           });
         return address;
     }
 
     // Create Offer on Near chain: exchange N Eth on Eth chain on M Near on Near chain.
-    async createOffer({derivedAddress, expectedAmount}) {
+    async createOffer({offerSalt, expectedAmount}) {
+        const derivedAddress = await this.getDerivedEthAddress(offerSalt);
         const res = await sendNearTransaction({
             nearSigner: this.account,
             contractId: NEAR_PROXY_ACCOUNT_ID,
@@ -56,40 +55,44 @@ export class JamonSwapClient {
     async checkDepositEth() {}
 
     // Buyer Withdraw his Eth via MPC contract.
-    async withdrawEth() {}
+    // TODO: separate logic from acceptOffer.
+    async withdrawByBuyer() {
+        throw new Error("Not implemented.");
+    }
 
-    // Seller Withdraw his Near.
-    async withdrawNear() {}
+    // Seller withdraw his Near after Buyer accepted the Offer.
+    async withdrawBySeller({offerSalt}) {
+        const derivedAddress = await this.getDerivedEthAddress(offerSalt);
+        const res = await sendNearTransaction({
+            nearSigner: this.account,
+            contractId: NEAR_PROXY_ACCOUNT_ID,
+            method: 'withdrawBySeller',
+            args: {
+                derivedAddress,
+            },
+            attachedDeposit: 0,
+        })
+        console.log(`[withdrawBySeller] Sent transaction: got ${res}`)
+    }
 
-    // The Buyer knew that Seller deposited on derived account Eth himself (or currently it fails with InsufficientFundsOnTargetChain)!
+    // The Buyer knew that Seller deposited on derived account Eth himself (or currently it fails with InsufficientFundsOnTargetChainError)!
     //  Thus, he is ready to accept the Offer:
     // Deposit Near on Near chain, and receive Eth from derived account for his address on Ethereum chain.
     // It is supposed that the buyer knew balance to transfer. TODO: relocate this logic inside.
-    async acceptOffer({derivedAddress, buyerEthAddress, amountEth}) {
+    async acceptOffer({offerSalt, buyerEthAddress, amountEth}) {
+        const derivedAddress = await this.getDerivedEthAddress(offerSalt);
+
+        // TODO: calculate amount to simplify the flow...
+
         await ethereum.send(
             {
             from: derivedAddress,
             to: buyerEthAddress,
             amount: amountEth,
-            mpcPath: MPC_PATH,
+            mpcPath: offerSalt,
             nearAccount: this.account,
             nearContractId: NEAR_PROXY_ACCOUNT_ID,
             derivedAddress,
         });
     }
-
-    // async createDeal() {
-    //     // TODO: Generate address on eth to deposit money.
-    //     // Use NEAR_PROXY_ACCOUNT_ID == our deployed contract that proxies requests to Jamon contract.
-    //     const generatedAddress = await generateAddress({
-    //         publicKey: "publicKey",
-    //         accountId: jamonContractAccountId,
-    //         path: "path",
-    //     })
-    //     console.log()
-    //
-    //
-    //
-    // }
-
 }
