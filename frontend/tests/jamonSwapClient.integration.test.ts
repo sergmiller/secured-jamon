96dc3b6547e3f7e70870d1153d1001cf5e3cca9a
AlcibiadesCleinias <ivan.cheprasov@phystech.edu>
2024-05-20 02:19:01 +0200

    tests

--- frontend/tests/jamonSwapClient.integration.test.ts
+++ frontend/tests/jamonSwapClient.integration.test.ts
@@ -8,16 +8,18 @@ const { Near, Account, keyStores, KeyPair } = nearAPI;
 const INTEGRATION_TEST_TIMEOUT = 300000000;
 
 // TODO: init account in upper levels.
-const NEAR_ACCOUNT_ID = import.meta.env.VITE_NEAR_ACCOUNT_ID ?? "";
-const NEAR_PRIVATE_KEY = import.meta.env.VITE_NEAR_PRIVATE_KEY ?? "";
+const NEAR_ACCOUNT_ID_SELLER = import.meta.env.VITE_NEAR_ACCOUNT_ID ?? "";
+const NEAR_PRIVATE_KEY_SELLER = import.meta.env.VITE_NEAR_PRIVATE_KEY ?? "";
 
-const accountId = NEAR_ACCOUNT_ID;
-const privateKey = NEAR_PRIVATE_KEY;
+const NEAR_ACCOUNT_ID_BUYER= import.meta.env.VITE_NEAR_ACCOUNT_ID_TEST_BUYER ?? "";
+const NEAR_PRIVATE_KEY_BUYER= import.meta.env.VITE_NEAR_PRIVATE_KEY_TEST_BUYER ?? "";
+
 const keyStore = new keyStores.InMemoryKeyStore();
-keyStore.setKey('testnet', accountId, KeyPair.fromString(privateKey));
+keyStore.setKey('testnet', NEAR_ACCOUNT_ID_SELLER, KeyPair.fromString(NEAR_PRIVATE_KEY_SELLER));
+keyStore.setKey('testnet', NEAR_ACCOUNT_ID_BUYER, KeyPair.fromString(NEAR_PRIVATE_KEY_BUYER));
 
 console.log('Near Chain Signature (NCS) call details:');
-console.log('Near accountId', accountId);
+console.log('Near Seller accountId', NEAR_ACCOUNT_ID_SELLER);
 
 const config = {
   networkId: 'testnet',
@@ -28,36 +30,41 @@ const config = {
   explorerUrl: 'https://testnet.nearblocks.io',
 };
 export const near = new Near(config);
-export const account = new Account(near.connection, accountId);
+const accountSeller = new Account(near.connection, NEAR_ACCOUNT_ID_SELLER);
+const accountBuyer = new Account(near.connection, NEAR_ACCOUNT_ID_BUYER);
 
 
 test("#jamonSwapClient", async () => {
-    const client = new JamonSwapClient(
-        account,
+    const clientSeller = new JamonSwapClient(
+        accountSeller,
     )
-    const offerId = "offerIdnear-phd-again.testnet.1716120580598"//client.generateOfferId(NEAR_ACCOUNT_ID)
+    const clientBuyer = new JamonSwapClient(
+        accountBuyer,
+    )
+    const offerId = "offerIdnear-phd-again.testnet.1716153474750"//client.generateOfferId(NEAR_ACCOUNT_ID)
     console.log('Generate offerId', offerId)
 
-    const derivedAddress = await client.getDerivedEthAddress(offerId)
+    const derivedAddress = await clientSeller.getDerivedEthAddress(offerId)
     console.log('Got Derived address: ', derivedAddress)
 
-    await client.createOffer(
+    await clientSeller.createOffer(
         {
             offerId,
-            expectedAmount: 1,
+            expectedAmount: 2,
         }
     )
 
     // TODO: check that Eth for derived account exists.
 
-    await client.acceptOffer(
+    await clientBuyer.acceptOffer(
     {
         offerId,
         buyerEthAddress: "0x460b414B401c5560a59784b7e71850890C28B213",
         amountEth: "0.0002",
+        amountDepositNear: "2",
     })
 
-    await client.withdrawBySeller(
+    await clientSeller.withdrawBySeller(
         {offerId}
     )
 }, INTEGRATION_TEST_TIMEOUT);
